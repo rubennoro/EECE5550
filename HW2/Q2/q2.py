c8dc7b5ea51a3e6891d705893da25b4d8ede3383
Ruben Noroian <rubennoro>
2025-10-28 18:19:02 -0400

    working q3 some parts

--- HW2/Q2/q2.py
+++ HW2/Q2/q2.py
@@ -2,6 +2,7 @@ print("Q2")
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.spatial import cKDTree
 
 file_path_x = 'pclX.txt'
 file_path_y = 'pclY.txt'
@@ -38,23 +39,31 @@ class ScanMatching:
         pass
 
     def RSME(self, X, Y, t_init, R_init, d_max, num_ICP_iters):
+        """
+        Contains all inputs for the functions below, as RMSE() runs ICP to get 
+        optimal translation and rotation outputs, and then calculates rmse.
+        """
         t, R, C = self.ICP(X, Y, t_init, R_init, d_max, num_ICP_iters)
         
         K = len(C)
-        rmse = 0
-        for k in C:
-            x_transformed = np.dot(R, X[k[0]]) + t
-            rmse += np.linalg.norm(Y[k[1]] - x_transformed)**2
-            rmse /= K
-            rmse = np.sqrt(rmse)
+        errs = []
+        for i, j in C:
+            x_transformed = R @ X[i] + t
+            rmses = np.linalg.norm(Y[j] - x_transformed)**2
+            errs.append(rmses)
+        rmse = np.sqrt(np.mean(errs))
         
         return t, R, rmse
     
     def ICP(self, X, Y, t, R, d_max, iters):
-        C = None
+        """
+        X and Y are input pointclouds.
+        t and R are the initial rigid transformations updated by ComputerOptimalRigidRegistration()
+        iters and d_max are params
+        """
         for i in range(iters):
             C = self.EstimateCorrespondences(X, Y, t, R, d_max)
-        t, R = self.ComputeOptimalRigidRegistration(X, Y, C)
+            t, R = self.ComputeOptimalRigidRegistration(X, Y, C)
 
         return t, R, C
     
@@ -69,6 +78,15 @@ class ScanMatching:
         C = [] # .append(i, j) for indices in X and Y that match
         nx = len(X)
 
+        tree = cKDTree(Y)
+        trans_x = (R @ X.T).T + t
+        dists, inds = tree.query(trans_x, distance_upper_bound=d_max)
+        for i in range(nx):
+            if dists[i] < d_max:
+                C.append([i, inds[i]])
+
+        '''
+        Runtime for Code below is too long, optimized with above version.
         # For each point in X, find the closest point in Y after transformation.
         for i in range(nx):
             
@@ -85,7 +103,7 @@ class ScanMatching:
                 
                 # Append the X, Y indices from each PCL 
                 C.append([i, j_min])
-
+        '''
         return C
     
     def ComputeOptimalRigidRegistration(self, X, Y, C):
@@ -103,35 +121,27 @@ class ScanMatching:
 
         # Centroids
         K = len(C)
-        x_hat = sum(X[k[0]] for k in C) / K
-        y_hat = sum(Y[k[1]] for k in C) / K
+        X_corr = np.array([X[i] for i, _ in C])  # Shape: (K, 3)
+        Y_corr = np.array([Y[j] for _, j in C])  # Shape: (K, 3)
 
+        x_hat = np.mean(X_corr, axis=0)
+        y_hat = np.mean(Y_corr, axis=0)
         # For each pair of indices in C, get the values associated with each of them
         # and center them. 
-        X_center = [X[k[0]] - x_hat for k in C]
-        Y_center = [Y[k[1]] - y_hat for k in C]
+        X_center = X_corr - x_hat
+        Y_center = Y_corr - y_hat
 
         # 3 x 3 cross-cov mat
-        W = np.zeros((len(X[0]), len(Y[0])))
-        for k in range(K):
-            
-            # This is the same as 3x1 * 1x3, where X gets transposed
-            W += np.outer(Y_center[k], X_center[k])
-
-        W /= K
-        print(W)
-
-        U, s, Vh = np.linalg.svd(W)
-
-        print(s)
-        v_reg = np.transpose(Vh)
-        det = np.linalg.det(np.dot(U, v_reg))
-        D = np.diag(np.concatenate([np.ones(2), [det]]))
+        cross_cov = (Y_center.T @ X_center) / K
         
-        R = np.dot(U, np.dot(D, Vh))
-        t = y_hat - np.dot(R, x_hat)
-        return t, R
+        U, s, Vt = np.linalg.svd(cross_cov)
+        
+        rot = U @ np.diag([1, 1, np.linalg.det(U @ Vt)]) @ Vt.T
+        trans = y_hat - rot @ x_hat
+        return trans, rot
     
+X = np.array(X)
+Y = np.array(Y)
 t_init = np.array([0, 0, 0])
 R_init = np.eye(3)
 d_max = 0.25
@@ -139,7 +149,14 @@ num_ICP_iters = 30
 p2 = ScanMatching()
 t, R, rmse = p2.RSME(X, Y, t_init, R_init, d_max, num_ICP_iters)
 
-x_transformed = [(np.dot(R, X[i]) + t) for i in range(X)]
+print("Translation:")
+print(t)
+print("Rotation")
+print(R)
+print("Root-Mean Squared Error: ")
+print(rmse)
+
+x_transformed = [((R @ X[i]) + t) for i in range(len(X))]
 
 # Plotting both point clouds on the same 3D axes
 fig = plt.figure()
